d69dc0fe6798372e458ccd259914810f97df5366
Abhishek Raj <abhishekraj272@gmail.com>
2021-05-22 12:43:11 +0530

    Updated icon

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,9 +4,9 @@ import './Header.scss';
 export default function Header() {
     return (
         <div className="header">
-            {/* <img src="" alt="Machaao Logo" /> */}
+            <img src="/covac-logo192.png" alt="Machaao Logo" />
 
-            <h3>Co-Vac</h3>
+            {/* <h3>Co-Vac</h3> */}
         </div>
     )
 }
